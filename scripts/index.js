54a2c2b88a4a40e043d9f4a9967225da706ab915
Marina Malyarchuk <pchupchu@yandex.ru>
2022-10-28 23:43:26 +0300

    верстка 5 пр

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,42 @@
-let editBtn = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeBtn = popup.querySelector('.popup__close-button');
+const editBtn = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const closeBtns = document.querySelectorAll('.popup__close-button');
 
-let profileName = document.querySelector('.profile__name');
-let profileDesc = document.querySelector('.profile__description');
+const profileName = document.querySelector('.profile__name');
+const profileDesc = document.querySelector('.profile__description');
 
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('#firstname');
-let jobInput = document.querySelector('#job');
+const formElement = document.querySelector('.form');
+const nameInput = document.querySelector('#firstname');
+const jobInput = document.querySelector('#job');
 
-function open() {
-  popup.classList.add('popup_opened');
+const addBtn = document.querySelector('.profile__add-button');
+const popupAddImage = document.querySelector('.popup_type_add-image');
+
+const likeBtn = document.querySelector('.element__like-button');
+
+
+
+// open and close edit form
+function openEditForm() {
+  popupEditProfile.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileDesc.textContent;
 }
 
-editBtn.addEventListener('click', open);
+editBtn.addEventListener('click', openEditForm);
 
 function close() {
-  popup.classList.remove('popup_opened');
+  const popupOpened = document.querySelector('.popup_opened');
+  popupOpened.classList.remove('popup_opened');
 }
 
-closeBtn.addEventListener('click', close);
+closeBtns.forEach((button) => {
+  button.addEventListener('click', close);
+})
+//closeBtn.addEventListener('click', close);
 
+// submit edit form
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -31,3 +45,28 @@ function formSubmitHandler (evt) {
 };
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+// open add image
+function openAddImage() {
+  popupAddImage.classList.add('popup_opened');
+}
+
+addBtn.addEventListener('click', openAddImage);
+
+
+// like button
+
+// trash button
+
+//open card
+const card = document.querySelector('.element__image');
+console.log(card);
+const popupImage = document.querySelector('.popup_type_image');
+console.log(popupImage);
+
+function openImage() {
+  popupImage.classList.add('popup_opened');
+}
+
+card.addEventListener('click', openImage);
+
