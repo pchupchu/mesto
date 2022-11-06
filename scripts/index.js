a2ffa72bee0e714a55d6dc4384fe88a669d55b7d
Marina Malyarchuk <pchupchu@yandex.ru>
2022-11-06 22:33:18 +0300

    добавление карточки, перебор массива

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const closeBtns = document.querySelectorAll('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileDesc = document.querySelector('.profile__description');
 
-const formElement = document.querySelector('.form');
+const formElementProfile = document.querySelector('.form_profile');
 const nameInput = document.querySelector('#firstname');
 const jobInput = document.querySelector('#job');
 
@@ -16,9 +16,14 @@ const popupAddImage = document.querySelector('.popup_type_add-image');
 const likeBtns = document.querySelectorAll('.element__like-button');
 const trashBtns = document.querySelectorAll('.element__trash-button');
 
-const images = document.querySelectorAll('.element__image');
 const popupImage = document.querySelector('.popup_type_image');
 const popupImageTitle = document.querySelector('.popup__image-title');
+
+const cardList = document.querySelector('.elements__list');
+
+const formElementCard = document.querySelector('.form_card');
+const imageNameInput = document.querySelector('#imagename');
+const imageUrlInput = document.querySelector('#imageurl');
 
 // open popup
 function openPopup(popupItem) {
@@ -52,7 +57,7 @@ function formSubmitHandler (evt) {
   close();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElementProfile.addEventListener('submit', formSubmitHandler);
 
 // open add image
 function openAddImage() {
@@ -61,33 +66,83 @@ function openAddImage() {
 
 addBtn.addEventListener('click', openAddImage);
 
-// like button
-likeBtns.forEach((button) => {
-  button.addEventListener('click', function(evt) {
+// add new card
+function addCard(cardName, cardUrl) {
+  const cardTemplate = document.querySelector('#template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+  cardElement.querySelector('.element__title').textContent = cardName;
+  cardElement.querySelector('.element__image').src = cardUrl;
+  cardElement.querySelector('.element__image').alt = cardName;
+
+  // like card
+  cardElement.querySelector('.element__like-button').addEventListener('click', function(evt) {
     evt.target.classList.toggle('element__like-button_active');
-  });
-})
-
-// trash button
-trashBtns.forEach((button) => {
-  button.addEventListener('click', function(evt) {
-    const cardItem = button.closest('.element');
-    cardItem.remove();
-  });
-})
-
-//open card
-images.forEach((item) => {
-  item.addEventListener('click', function(evt) {
-    const cardItem = item.closest('.element');
-    openPopup(popupImage);
-
-    const imageTitle = cardItem.querySelector('.element__title');
-    popupImageTitle.textContent = imageTitle.textContent;
-
-    const imageCard = document.querySelector('.popup__image-card');
-    const image = cardItem.querySelector('.element__image');
-    imageCard.src = image.src;
   })
- })
 
+  // delete card
+  cardElement.querySelector('.element__trash-button').addEventListener('click', function(evt) {
+    const cardItem = cardElement.querySelector('.element__trash-button').closest('.element');
+    cardItem.remove();
+  })
+
+  // open card
+  const imageElement = cardElement.querySelector('.element__image');
+  imageElement.addEventListener('click', function(evt) {
+
+      const cardItem = imageElement.closest('.element');
+      openPopup(popupImage);
+
+      const imageTitle = cardItem.querySelector('.element__title');
+      popupImageTitle.textContent = imageTitle.textContent;
+
+      const imageCard = document.querySelector('.popup__image-card');
+      const image = cardItem.querySelector('.element__image');
+      imageCard.src = image.src;
+      imageCard.alt = image.alt;
+    })
+
+  cardList.prepend(cardElement);
+}
+
+function cardSubmitHandler (evt) {
+  evt.preventDefault();
+  addCard(imageNameInput.value, imageUrlInput.value)
+  imageNameInput.value = '';
+  imageUrlInput.value = '';
+  close();
+}
+
+formElementCard.addEventListener('submit', cardSubmitHandler);
+
+// initial cards
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach((item) => {
+  addCard(item.name, item.link);
+});
